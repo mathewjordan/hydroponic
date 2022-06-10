4cd49d6ed3f52c90e6443c8bd11d3c85bae9c195
Mat Jordan <mat@northwestern.edu>
2022-06-10 13:41:07 -0400

    Create linrothe example.

---
+++ seeds/tankas-voyager-linrothe.ts
@@ -0,0 +1,50 @@
+export const instance = {
+  id: "http://127.0.0.1:8080/output/tankas-voyager-linrothe.json",
+  label: { none: ["Related items"] },
+  summary: {
+    none: [
+      "Search results for `voyager linrothe` and the <strong>Genre</strong> `tankas (scrolls or banners)`",
+    ],
+  },
+  homepage: [
+    {
+      id: "https://dc.library.northwestern.edu/search?q=%22voyager+linrothe%22&results=1&Genre=%5B%22tankas+%28scrolls+or+banners%29%22%5D",
+      type: "Text",
+      label: { none: "Related items" },
+      format: "text/html",
+    },
+  ],
+};
+
+export const items = [
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/f3/6a/58/a7/-3/2a/0-/42/34/-a/1d/e-/95/c7/e2/4c/d8/a4-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/57/20/c4/a7/-d/c7/5-/43/ee/-b/cc/5-/ed/20/f1/fa/6f/ac-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/66/5a/1d/bb/-4/cf/3-/4b/0d/-8/a9/f-/91/52/c6/b8/b2/d0-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/9b/b0/6e/56/-c/85/8-/4c/b8/-8/08/b-/a3/16/53/97/ef/c4-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/3e/9c/f6/32/-6/1e/6-/4b/47/-a/92/b-/0f/73/ae/72/68/cd-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/08/cb/1e/92/-3/14/8-/42/6e/-a/60/6-/48/18/10/63/4a/93-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/fc/b0/cd/8f/-2/5c/3-/4e/ec/-a/f7/8-/52/63/9a/8f/fa/41-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/d6/4b/68/55/-1/e1/b-/4b/aa/-9/14/f-/5f/f6/ed/c1/c2/29-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/72/dd/69/71/-d/bb/e-/45/ea/-9/d2/e-/fc/31/ec/d4/a7/1c-manifest.json",
+  },
+  {
+    id: "https://iiif.stack.rdc.library.northwestern.edu/public/c1/f0/8b/5b/-e/8f/1-/42/0d/-9/82/f-/45/07/55/74/66/23-manifest.json",
+  },
+];
--- src/dev.tsx
+++ src/dev.tsx
@@ -3,54 +3,18 @@ import ReactDOM from "react-dom";
 import { Hydroponic } from "./index";
 import { Collection } from "@iiif/presentation-3";
 import Bloom from "@samvera/bloom-iiif";
-
-const instance = {
-  id: "http://127.0.0.1:8080/output/mixed-source.json",
-  label: { none: ["Sample"] },
-  summary: { none: ["Sample"] },
-  homepage: [
-    {
-      id: "https://foo.bar/baz",
-      type: "Text",
-      label: { none: "Sample" },
-      format: "text/html",
-    },
-  ],
-};
-
-const items = [
-  {
-    id: "https://iiif.stack.rdc.library.northwestern.edu/public/7e/56/09/dc/-a/b7/1-/49/8a/-8/69/3-/19/6e/a3/8c/8b/ca-manifest.json",
-    href: "https://foo.bar/baz/qux",
-  },
-  {
-    id: "https://iiif.stack.rdc.library.northwestern.edu/public/b5/2d/5d/93/-a/11/7-/43/e9/-9/0c/7-/43/4f/a1/21/2b/60-manifest.json",
-    href: "https://foo.bar/baz/qux",
-  },
-  {
-    id: "https://iiif.stack.rdc.library.northwestern.edu/public/16/66/86/99/-7/0a/8-/48/8a/-9/63/3-/12/27/d7/86/f7/13-manifest.json",
-    href: "https://foo.bar/baz/qux",
-  },
-  {
-    id: "https://iiif.stack.rdc.library.northwestern.edu/public/f3/4e/ec/84/-2/37/1-/45/a3/-b/85/7-/bd/f1/95/93/3e/4f-manifest.json",
-    href: "https://foo.bar/baz/qux",
-  },
-  {
-    id: "https://iiif.stack.rdc.library.northwestern.edu/public/df/9a/96/dc/-7/66/d-/4b/a5/-b/f9/1-/fe/29/4e/1e/05/cb-manifest.json",
-    href: "https://foo.bar/baz/qux",
-  },
-  {
-    id: "https://iiif.stack.rdc.library.northwestern.edu/public/47/d2/76/f6/-f/a2/2-/49/91/-9/bf/3-/98/b3/f6/8f/0b/c5-manifest.json",
-    href: "https://foo.bar/baz/qux",
-  },
-];
+import { instance, items } from "../seeds/tankas-voyager-linrothe";
 
 const Wrapper: React.FC = () => {
+  /**
+   * Instantiate Hydroponic and grow the instance.
+   */
   const hydroponic = new Hydroponic();
   const collection = hydroponic.grow(instance, items) as Collection;
+
   return (
     <>
-      <Bloom collectionId="http://127.0.0.1:8080/output/mixed-source.json" />
+      <Bloom collectionId="http://127.0.0.1:8080/output/tankas-voyager-linrothe.json" />
       <textarea
         disabled
         style={{ width: "100%", height: "50vh" }}
